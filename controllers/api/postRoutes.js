c66ba0fb32ba61d78fa87626d1bc8506c5b607fc
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-01 07:45:19 -0500

    Add put route for posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,6 +16,35 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+//edit a post when signed in
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        // All the fields you can update and the data attached to the request body.
+        title: req.body.title,
+        contents: req.body.contents,
+      },
+      {
+        // Gets the post to target based on post id and user id
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+      
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //delete a post only when signed in
 router.delete('/:id', withAuth, async (req, res) => {
   try {
@@ -26,7 +55,7 @@ router.delete('/:id', withAuth, async (req, res) => {
       },
     });
 
-    if (!posttData) {
+    if (!postData) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
